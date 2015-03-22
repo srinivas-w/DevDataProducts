12e757a0a25d12adc7e112e620929b3324e0e1d4
srinivas reddy t <srinivas.tatireddi@wisseninfotech.com>
2015-03-22 09:40:23 +0530

    First Commit
    First Commit

---
+++ Assignment1/Detail.md
@@ -0,0 +1,7 @@
+This Shiny App is for searching and visulizating LEGO Sets information from year 1950 to 2015. The total number of set records is 9944 and that of theme records is 98.
+The dataset is from Rebrickable.com that contains the basic information of each set (set id, year, number of pieces, theme, set name).
+
+Data Source: http://rebrickable.com/downloads
+Note: Since I didn't use the API to collect data, the current data might not be 100% accurate.
+
+About author: SRINIVAS REDDY T
---
+++ Assignment1/Server.R
@@ -0,0 +1,97 @@
+library(shiny)
+# Load data processing file
+source("data_processing.R")
+themes <- sort(unique(data$theme))
+# Shiny server
+shinyServer(
+function(input, output) {
+# output$text1 <- renderText({input$text1})
+# output$text2 <- renderText({input$text2})
+# output$text3 <- renderText({
+# input$goButton
+# isolate(paste(input$text1, input$text2))
+# })
+output$setid <- renderText({input$setid})
+output$address <- renderText({
+input$goButtonAdd
+isolate(paste("http://brickset.com/sets/",
+input$setid, sep=""))
+})
+output$address2 <- renderText({
+input$goButtonDirect
+isolate(browseURL(paste("http://brickset.com/sets/",
+input$setid, sep="")))
+})
+# observe({
+# if(length(input$setid) > 0) {
+# output$address2 <- renderText({
+# input$goButtonDirect
+# isolate(browseURL(paste("http://brickset.com/sets/",
+# input$setid, sep="")))
+# })
+# }
+#
+# })
+# Initialize reactive values
+values <- reactiveValues()
+values$themes <- themes
+# Create event type checkbox
+output$themesControl <- renderUI({
+checkboxGroupInput('themes', 'LEGO Themes:',
+themes, selected = values$themes)
+})
+# Add observer on select-all button
+observe({
+if(input$selectAll == 0) return()
+values$themes <- themes
+})
+# Add observer on clear-all button
+observe({
+if(input$clearAll == 0) return()
+values$themes <- c() # empty list
+})
+# Prepare dataset
+dataTable <- reactive({
+groupByTheme(data, input$timeline[1],
+input$timeline[2], input$pieces[1],
+input$pieces[2], input$themes)
+})
+dataTableByYear <- reactive({
+groupByYearAgg(data, input$timeline[1],
+input$timeline[2], input$pieces[1],
+input$pieces[2], input$themes)
+})
+dataTableByPiece <- reactive({
+groupByYearPiece(data, input$timeline[1],
+input$timeline[2], input$pieces[1],
+input$pieces[2], input$themes)
+})
+dataTableByPieceAvg <- reactive({
+groupByPieceAvg(data, input$timeline[1],
+input$timeline[2], input$pieces[1],
+input$pieces[2], input$themes)
+})
+dataTableByPieceThemeAvg <- reactive({
+groupByPieceThemeAvg(data, input$timeline[1],
+input$timeline[2], input$pieces[1],
+input$pieces[2], input$themes)
+})
+# Render data table
+output$dTable <- renderDataTable({
+dataTable()
+} #, options = list(bFilter = FALSE, iDisplayLength = 50)
+)
+output$themesByYear <- renderChart({
+plotThemesCountByYear(dataTableByYear())
+})
+output$piecesByYear <- renderChart({
+plotPiecesByYear(dataTableByPiece())
+})
+output$piecesByYearAvg <- renderChart({
+plotPiecesByYearAvg(dataTableByPieceAvg())
+})
+output$piecesByThemeAvg <- renderChart({
+plotPiecesByThemeAvg(dataTableByPieceThemeAvg())
+})
+} # end of function(input, output)
+)
